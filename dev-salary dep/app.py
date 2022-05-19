5153253c8f12f4dbb5fe353ed9958940d8f0f175
lawalAfeez820 <69974956+lawalAfeez820@users.noreply.github.com>
2022-05-19 03:40:48 -0700

    Delete dev-salary dep directory

--- dev-salary dep/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from predict_page import show_predict_page
-from explore_page import show_explore_page
-
-new_page=st.sidebar.selectbox("Predict or Explore",("Predict","Explore"))
-if new_page=="Predict":
-    show_predict_page()
-else:
-    show_explore_page()
